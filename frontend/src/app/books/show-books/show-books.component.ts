0822c8d875bf906ae4ba36f4ccab5209d2a170e8
ViktoD <90144863+ViktoD@users.noreply.github.com>
2022-11-23 20:12:18 +0200

    Update show-books.component.ts

--- frontend/src/app/books/show-books/show-books.component.ts
+++ frontend/src/app/books/show-books/show-books.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Book } from 'src/app/models/book';
 import { SharedService } from 'src/app/shared.service';
 @Component({
   selector: 'app-show-books',
